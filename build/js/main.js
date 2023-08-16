b6192bd4385beb0b2d18f788d051240f9dc11559
tnarasimhan <thanigaivel.narasimhan@ul.com>
2023-08-16 16:30:24 -0400

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+var username = 'Jhon';
+console.log(username);
+function add(x, y) {
+    return x + y;
+}
+const result = add(10, 20);
+console.log(result); // 30
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+var username = 'Jhon';
+console.log(username);
+
+function add(x: number, y: number): number {
+    return x + y;
+  }
+  
+  const result = add(10, 20);
+  console.log(result); // 30
+
+
+
+
+
